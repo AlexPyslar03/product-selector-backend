93a99675101a4fbeba99291ee1c7d936d4b874e1
Александр Пысларь <62560925+AlexPyslar03@users.noreply.github.com>
2024-08-05 13:10:34 +0300

    init spring project

---
+++ src/main/java/com/alexpyslar03/productselectorbackend/ProductSelectorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.alexpyslar03.productselectorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSelectorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductSelectorBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alexpyslar03/productselectorbackend/ProductSelectorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexpyslar03.productselectorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSelectorBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
