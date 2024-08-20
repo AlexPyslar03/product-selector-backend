853f06389b85ef0019b673ccfefcc6703608d86b
Александр Пысларь <62560925+AlexPyslar03@users.noreply.github.com>
2024-08-20 22:17:37 +0300

    Проверка тестов

--- src/test/java/com/alexpyslar03/productselectorbackend/repository/UserRepositoryTest.java
+++ src/test/java/com/alexpyslar03/productselectorbackend/repository/UserRepositoryTest.java
@@ -31,7 +31,6 @@ public class UserRepositoryTest {
         user1 = userRepository.save(User.builder()
                 .name("John")
                 .surname("Doe")
-                .email("john.doe@example.com")
                 .password("password123")
                 .birthDate(LocalDate.of(1990, 1, 1))
                 .registrationDate(LocalDate.of(1990, 1, 2))
